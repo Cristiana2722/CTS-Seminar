2dbfbce8b4a9e7119d96f3e1c8c2eee2314e7739
Cristiana <cristiana071201@gmail.com>
2024-02-26 19:42:42 +0200

    Applied basic rules of Clean Code

--- CTS_S01/src/ro/ase/cts/s01/Main.java
+++ CTS_S01/src/ro/ase/cts/s01/Main.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Calculator calculator = new Calculator();
-        double result = calculator.calculateX5PlusY7();
+        double result = calculator.addition();
         System.out.println("Result: " + result);
-
     }
 }
