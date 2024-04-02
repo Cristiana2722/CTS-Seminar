9b6e12c2e31db7d830e70a00a79cf068b977f06b
Cristiana <cristiana071201@gmail.com>
2024-04-02 19:57:26 +0300

    Homework - Prototype Implementation

---
+++ PrototypeHw/src/ro/ase/cts/prototype/Main.java
@@ -0,0 +1,9 @@
+package ro.ase.cts.prototype;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ReportGenerator reportGenerator = new ReportGenerator();
+        Report salesReport = reportGenerator.generateReport("Data", "PDF", "Sales");
+        salesReport = reportGenerator.generateReport("Data", "PDF", "Sales");
+    }
+}
